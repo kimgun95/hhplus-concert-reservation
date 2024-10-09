1e1f78f2a05c7cb31cebd384a9ab24c759e669b2
kimgun95 <harrison1995@naver.com>
2024-10-09 16:48:08 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/hhplus/concertreservation/ConcertreservationApplication.java
@@ -0,0 +1,13 @@
+package hhplus.concertreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hhplus/concertreservation/ConcertreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.concertreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
