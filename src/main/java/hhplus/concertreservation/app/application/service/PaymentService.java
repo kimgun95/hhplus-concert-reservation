6049a0956afe7c950448275eb6198a6771b0645a
kimgun95 <harrison1995@naver.com>
2024-10-24 23:58:14 +0900

    refactor: 결제 실패시 예약/좌석의 상태 변화가 db에 적용되지 않는 점 수정 - 기존에 에러를 catch 한 뒤 예약/좌석의 상태를 변경시켰으나 마지막에 다시 에러를 throw하여 트랜잭션 커밋이 아닌 롤백이 되는 현상을 발견했습니다. - 에러를 다시 throw 하지 않음으로써 트랜잭션이 커밋되게 만들었습니다. - 결제 실패시에도 payment(결제 내역)을 생성하도록 구현했습니다.

--- src/main/java/hhplus/concertreservation/app/application/service/PaymentService.java
+++ src/main/java/hhplus/concertreservation/app/application/service/PaymentService.java
@@ -6,12 +6,11 @@ import hhplus.concertreservation.app.domain.constant.SeatStatus;
 import hhplus.concertreservation.app.domain.constant.TransactionType;
 import hhplus.concertreservation.app.domain.entity.*;
 import hhplus.concertreservation.app.domain.repository.*;
-import hhplus.concertreservation.config.exception.ErrorCode;
 import hhplus.concertreservation.config.exception.FailException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -28,28 +27,21 @@ public class PaymentService {
     private final UsersRepository usersRepository;
 
     @Transactional
-    public Payment useUserPoint(Long userId, Long reservationId, Long amount) {
+    public Payment payment(Long userId, Long reservationId, Long amount) {
 
         Reservation reservation = Reservation.getOrThrowIfNotFound(reservationRepository.findById(reservationId));
+        Users user = Users.getOrThrowIfNotFound(usersRepository.findById(userId));
         log.info("좌석 예약 요청 유저의 ID : {}", reservation.getUserId());
 
         try {
             // 대기열 만료 확인 (만료된 토큰은 이미 스케줄러에 의해 삭제됨)
             Queue queue = Queue.getOrThrowIfNotFound(queueRepository.findByUserId(userId));
-            System.out.println("--------------------대기열 토큰 확인");
-            Users user = Users.getOrThrowIfNotFound(usersRepository.findById(userId));
-            System.out.println("--------------------유저 확인");
-
             // 유저 포인트 사용 (유저의 남은 잔액부터 확인)
             user.usePoints(amount);
             // 결제 내역 생성
             Payment payment = paymentRepository.save(Payment.create(user.getId(), reservationId, amount, PaymentStatus.SUCCESS));
-            System.out.println("--------------------결제 내역 생성");
-
             // 원장 생성
             ledgerRepository.save(Ledger.create(userId, amount, TransactionType.USE));
-            System.out.println("--------------------원장 생성");
-
             // 예약 상태 성공으로 변환
             reservation.changeStatus(ReservationStatus.SUCCESS);
             // 만료 시키기 위해 만료 시간 변경
@@ -63,9 +55,9 @@ public class PaymentService {
             reservation.changeStatus(ReservationStatus.FAILED);
             // 좌석 예약 가능 상태로 변환
             Seat seat = Seat.getOrThrowIfNotFound(seatRepository.findById(reservation.getSeatId()));
-            System.out.println("--------------------좌석 조회 확인");
             seat.changeStatus(SeatStatus.AVAILABLE);
-            throw new FailException(ErrorCode.PAYMENT_FAILED);
+            // 결제 내역 생성
+            return paymentRepository.save(Payment.create(user.getId(), reservationId, amount, PaymentStatus.FAILED));
         }
     }
 }
