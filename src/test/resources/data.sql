d9cb84db01e0945207cc4e811c6bb2bb16544005
kimgun95 <harrison1995@naver.com>
2024-10-30 10:34:42 +0900

    feat: 좌석 예약에 대한 동시성 제어 통합 테스트 - h2 db를 통합 테스트 환경으로 설정 - '여러명의 사람이 하나의 좌석에 대해 동시에 예약 요청'하는 시나리오 작성

---
+++ src/test/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO concert (concert_name, concert_date) VALUES ('LOA ON WINTER', '2024-12-01 19:00:00');
+
+INSERT INTO seat (concert_id, seat_number, seat_status) VALUES (1, 1, 'AVAILABLE');
+INSERT INTO seat (concert_id, seat_number, seat_status) VALUES (1, 2, 'AVAILABLE');
+INSERT INTO seat (concert_id, seat_number, seat_status) VALUES (1, 3, 'AVAILABLE');
+
+INSERT INTO users (user_name, user_point) VALUES ('user1', 10000);
+INSERT INTO users (user_name, user_point) VALUES ('user2', 10000);
+INSERT INTO users (user_name, user_point) VALUES ('user3', 10000);
+INSERT INTO users (user_name, user_point) VALUES ('user4', 10000);
+INSERT INTO users (user_name, user_point) VALUES ('user5', 10000);
