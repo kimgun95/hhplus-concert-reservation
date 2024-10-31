4bb9c8dea933fc0d3a1cb5dcb78f95c521e79bb3
kimgun95 <harrison1995@naver.com>
2024-10-31 12:00:54 +0900

    refactor: ErrorCode에 HttpStatus 필드 추가 에러 Status로 FailException인 경우에 200이 떨어지는데, ErrorCode에서 HttpStatus를 넣어줘 적당한 Http 응답 Status를 활용하려 시도. HTTP Status에서 400 번 대의 에러를 위주로 학습.

--- src/main/java/hhplus/concertreservation/config/exception/ErrorCode.java
+++ src/main/java/hhplus/concertreservation/config/exception/ErrorCode.java
@@ -2,21 +2,22 @@ package hhplus.concertreservation.config.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
 
-    EXPIRED_QUEUE_TOKEN("E100", "만료된 토큰. 대기열 토큰을 반급받아 주세요."),
-    USER_NOT_FOUND("E101", "존재하지 않는 유저입니다."),
-    SEAT_NOT_FOUND("E102", "존재하지 않는 좌석입니다."),
-    RESERVED_SEAT("E103", "이미 예약된 좌석입니다."),
-    USER_POINT_NOT_ENOUGH("E104", "사용하려는 포인트가 부족합니다."),
-    RESERVATION_NOT_FOUND("E105", "존재하지 않는 예약입니다."),
-    PAYMENT_FAILED("E106", "결제가 실패했습니다."),
-    INVALID_QUEUE_STATUS("E107", "API 요청이 가능한 대기열 상태가이 아닙니다"),
+    EXPIRED_QUEUE_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰. 대기열 토큰을 반급받아 주세요."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+    SEAT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 좌석입니다."),
+    RESERVED_SEAT(HttpStatus.CONFLICT, "이미 예약된 좌석입니다."),
+    USER_POINT_NOT_ENOUGH(HttpStatus.CONFLICT, "사용하려는 포인트가 부족합니다."),
+    RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 예약입니다."),
+    PAYMENT_FAILED(HttpStatus.CONFLICT, "결제가 실패했습니다."),
+    INVALID_QUEUE_STATUS(HttpStatus.UNAUTHORIZED, "API 요청이 가능한 대기열 상태가이 아닙니다"),
     ;
 
-    private final String code;
+    private final HttpStatus httpStatus;
     private final String message;
 }
