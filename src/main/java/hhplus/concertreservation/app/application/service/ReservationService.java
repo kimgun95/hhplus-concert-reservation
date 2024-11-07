8d3f12afbe2c0e0ea67f44526b913b264d7b6d82
kimgun95 <harrison1995@naver.com>
2024-11-07 15:33:42 +0900

    fix: redis를 이용한 분산락 기능 제거

--- src/main/java/hhplus/concertreservation/app/application/service/ReservationService.java
+++ src/main/java/hhplus/concertreservation/app/application/service/ReservationService.java
@@ -9,9 +9,6 @@ import hhplus.concertreservation.app.domain.entity.Seat;
 import hhplus.concertreservation.app.domain.repository.QueueRepository;
 import hhplus.concertreservation.app.domain.repository.ReservationRepository;
 import hhplus.concertreservation.app.domain.repository.SeatRepository;
-import hhplus.concertreservation.app.infrastructure.RedissonLockExecutor;
-import hhplus.concertreservation.config.exception.ErrorCode;
-import hhplus.concertreservation.config.exception.FailException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,29 +26,20 @@ public class ReservationService {
     private final QueueRepository queueRepository;
     private final QueueChecker queueChecker;
     private final SeatChecker seatChecker;
-    private final RedissonLockExecutor redissonLockExecutor;
+//    private final RedissonLockExecutor redissonLockExecutor;
 
     @Transactional
     public Reservation reserveSeat(Long userId, Long concertId, Long seatNumber) {
-        try {
-            return redissonLockExecutor.execute(
-                    "seat-lock:" + concertId + "-" + seatNumber,
-                    () -> {
-                        Seat seat = seatChecker.getOrThrowIfNotFound(seatRepository.findByConcertIdAndSeatNumber(concertId, seatNumber));
-                        // AVAILABLE이 아니라면 예외
-                        seatChecker.validateIfAvailable(seat);
-                        // 대기열 5분 설정
-                        Queue queue = queueChecker.getOrThrowIfNotFound(queueRepository.findByUserId(userId));
-                        queue.changeExpiredAt(LocalDateTime.now().plusMinutes(5));
-                        log.info("좌석 예약 요청 유저의 ID : {}", queue.getUserId());
-                        // 좌석 예약 상태로 변환
-                        seat.changeStatus(SeatStatus.RESERVED);
-                        // 예약 생성
-                        return reservationRepository.save(Reservation.create(queue.getUserId(), seat.getId()));
-                    }
-            );
-        } catch (Exception e) {
-            throw new FailException(ErrorCode.REDIS_LOCK_POSSESSION_FAILED, FailException.LogLevel.WARN);
-        }
+        Seat seat = seatChecker.getOrThrowIfNotFound(seatRepository.findByConcertIdAndSeatNumber(concertId, seatNumber));
+        // AVAILABLE이 아니라면 예외
+        seatChecker.validateIfAvailable(seat);
+        // 대기열 5분 설정
+        Queue queue = queueChecker.getOrThrowIfNotFound(queueRepository.findByUserId(userId));
+        queue.changeExpiredAt(LocalDateTime.now().plusMinutes(5));
+        log.info("좌석 예약 요청 유저의 ID : {}", queue.getUserId());
+        // 좌석 예약 상태로 변환
+        seat.changeStatus(SeatStatus.RESERVED);
+        // 예약 생성
+        return reservationRepository.save(Reservation.create(queue.getUserId(), seat.getId()));
     }
 }
---
+++ src/main/java/hhplus/concertreservation/config/configuration/RedissonConfig.java
@@ -0,0 +1,23 @@
+//package hhplus.concertreservation.config.configuration;
+//
+//import org.redisson.Redisson;
+//import org.redisson.api.RedissonClient;
+//import org.redisson.config.Config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RedissonConfig {
+//
+//    private String redisHost = "localhost";
+//
+//    private int redisPort = 6379;
+//
+//    @Bean
+//    public RedissonClient redissonClient() {
+//        Config config = new Config();
+//        config.useSingleServer()
+//                .setAddress("redis://" + redisHost + ":" + redisPort);
+//        return Redisson.create(config);
+//    }
+//}
